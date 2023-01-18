90417d59436fc4345e209079f48254e2487968a4
Andrew Roberts <arober@bu.edu>
2023-01-18 17:06:20 -0500

    Generalized prior functions to allow product inverse Gamma or inverse Wishart

--- vsem_calibration_tests/mcmc_calibration_functions.r
+++ vsem_calibration_tests/mcmc_calibration_functions.r
@@ -65,7 +65,8 @@ llik_Gaussian_err <- function(model_errs, Sig_eps, output_vars) {
   #
   # Args:
   #     model_errs: matrix of dimensions n x p, where n = number observations in time 
-  #                 series and p = number output variables. 
+  #                 series and p = number output variables. This is the model error matrix
+  #                 Y - f(theta). 
   #     Sig_eps: matrix of dimensions p x p, the covariance matrix used in the 
   #              multivariate Gaussian likelihood calculation. 
   #
@@ -148,7 +149,7 @@ calc_lprior_theta <- function(theta, theta_prior_params) {
 
 mcmc_calibrate <- function(par_ref, par_cal_sel, data_obs, output_vars, PAR,
                            theta_init = NA, theta_prior_params, 
-                           learn_Sig_eps = FALSE, Sig_eps_init = NA, Sig_eps_prior_params = NA,
+                           learn_Sig_eps = FALSE, Sig_eps_init = NA, Sig_eps_prior_params = NA, diag_cov = FALSE, 
                            N_mcmc, adapt_frequency, adapt_min_scale, accept_rate_target) {
   # MCMC implementation for VSEM carbon model. Accommodates Gaussian likelihood, possibly with correlations 
   # between the different output variables, but assumes independence across time. Samples from posterior over both  
@@ -177,12 +178,13 @@ mcmc_calibrate <- function(par_ref, par_cal_sel, data_obs, output_vars, PAR,
   #                        "Gaussian" (param1 = mean, param2 = std dev) and "Uniform" (param1 = lower, param2 = upper).
   #    learn_Sig_eps: logical, if TRUE treats observation covariance matrix as random and MCMC samples from joint 
   #                   posterior over Sig_eps and theta. Otherwise, fixes Sig_eps at value `Sig_eps_init`.
-  #    Sig_eps_init: matrix, p x p covariance matrix capturing dependence beteen output variables. If `learn_Sig_eps` is 
+  #    Sig_eps_init: matrix, p x p covariance matrix capturing dependence between output variables. If `learn_Sig_eps` is 
   #                  TRUE then `Sig_eps_init` can either be set to the initial value used for MCMC, or set to NA in which 
   #                  case the initial value will be sampled from the prior. If `learn_Sig_eps` is FALSE, then a non-NA value 
   #                  is required, and treated as the fixed nominal value of Sig_eps_init. 
-  #    Sig_eps_prior_params: list, containing elements with names "scale_matrix" and "dof" 
-  #                          that are the arguments of the Inverse Wishart distribution. Only required if `learn_Sig_eps` is TRUE.
+  #    Sig_eps_prior_params: list, defining prior on Sig_eps. See `sample_prior_Sig_eps()` for details. Only required if `learn_Sig_eps` is TRUE.
+  #    diag_cov: logical, if TRUE constrains Sig_eps to be diagonal. If the prior distribution is specified to be product Inverse Gamma then this 
+  #              is automatically set to TRUE. Default is FALSE. 
   #    N_mcmc: integer, the number of MCMC iterations. 
   #    adapt_frequency: integer, number of iterations in between each covariance adaptation. 
   #    min_scale: numeric scalar, used as a floor for the scaling factor in covariance adaptation, see `adapt_cov_proposal()`.
@@ -203,7 +205,14 @@ mcmc_calibrate <- function(par_ref, par_cal_sel, data_obs, output_vars, PAR,
   par_cal_names <- rownames(par_ref)[par_cal_sel]
   theta_samp <- matrix(nrow = N_mcmc, ncol = par_cal_sel)
   colnames(theta_samp) <- par_cal_names
-  Sig_eps_samp <- matrix(nrow = N_mcmc, ncol = 0.5*p*(p+1)) # Each row stores lower triangle of Sig_eps
+  if(isTRUE(Sig_eps_prior_params$dist == "IG")) {
+    diag_cov <- TRUE
+  }
+  if(diag_cov) {
+    Sig_eps_samp <- matrix(nrow = N_mcmc, ncol = p)
+  } else {
+    Sig_eps_samp <- matrix(nrow = N_mcmc, ncol = 0.5*p*(p+1)) # Each row stores lower triangle of Sig_eps
+  }
 
   # Set initial values
   if(is.na(theta_init)) {
@@ -214,7 +223,12 @@ mcmc_calibrate <- function(par_ref, par_cal_sel, data_obs, output_vars, PAR,
   }
   
   theta_samp[1,] <- theta_init
-  Sig_eps_samp[1,] <- Sig_eps_init
+  if(diag_cov) {
+    Sig_eps_samp[1,] <- diag(Sig_eps_init)
+  } else {
+    Sig_eps_samp[1,] <- lower.tri(Sig_eps_init, diag = TRUE)
+  }
+  
   Sig_eps_curr <- Sig_eps_init
   model_errs_curr <- data_obs - run_VSEM(theta_init, par_ref, par_cal_sel, PAR)
   log_theta_post_curr <- llik_Gaussian_err(model_errs_curr, Sig_eps_curr, output_vars) + calc_lprior_theta(theta_init, theta_prior_params)
@@ -261,9 +275,12 @@ mcmc_calibrate <- function(par_ref, par_cal_sel, data_obs, output_vars, PAR,
     #
 
     if(learn_Sig_eps) {
-      Sig_eps_curr <- sample_Sig_eps()
-      Sig_eps_curr <- sample_Sig_eps(model_errs_curr, Sig_eps_prior_params, n)
-      Sig_eps_samp[itr,] <- lower.tri(Sig_eps_curr, diag = TRUE)
+      Sig_eps_curr <- sample_cond_post_Sig_eps(model_errs_curr, Sig_eps_prior_params, n)
+      if(diag_cov) {
+        Sig_eps_samp[itr,] <- diag(Sig_eps_curr)
+      } else {
+        Sig_eps_samp[itr,] <- lower.tri(Sig_eps_curr, diag = TRUE)
+      }
     }
 
   }
@@ -300,42 +317,69 @@ sample_prior_theta <- function(theta_prior_params) {
 
 
 sample_prior_Sig_eps <- function(Sig_eps_prior_params) {
-  # Returns sample from prior Inverse Wishart distribution on the p x p observation covariance matrix Sig_eps
+  # Returns sample from prior distribution on the p x p observation covariance matrix Sig_eps
   #
   # Args:
-  #    Sig_eps_prior_params: list, containing elements with names "scale_matrix" and "dof" 
-  #                          that are the arguments of the Inverse Wishart distribution.
+  #    Sig_eps_prior_params: list, must contain element named "dist" specifying the prior distribution. Current accepted values are 
+  #                          "IW" (Inverse Wishart) or "IG" (independent Inverse Gamma priors). Depending on value of "dist", 
+  #                          must also contain either either 1.) names "scale_matrix" and "dof" 
+  #                          that are the arguments of the Inverse Wishart distribution, or 2.) names "IG_shape" and "IG_scale" which 
+  #                          each correspond to p-length vectors storing the parameters for the independent Inverse Gamma priors on each
+  #                          variance parameter. Note that dist "IG" constrains Sig_eps to be diagonal, while "IW" does not. 
   #
   # Returns:
-  #    matrix, p x p positive definite matrix sampled from the inverse Wishart prior p(Sig_eps).
+  #    matrix, p x p positive definite matrix sampled from the prior p(Sig_eps).
   
-  return(LaplacesDemon::rinvwishart(nu = Sig_eps_prior_params$dof, S = Sig_eps_prior_params$scale_matrix))
+  if(Sig_eps_prior_params$dist == "IW") {
+    return(LaplacesDemon::rinvwishart(nu = Sig_eps_prior_params$dof, S = Sig_eps_prior_params$scale_matrix))
+  } else if(Sig_eps_prior_params$dist == "IG") {
+    sig2_eps_vars <- vector(mode = "numeric", length = p)
+    for(j in seq_len(p)) {
+      sig2_eps_vars[j] <- LaplacesDemon::rinvgamma(1, shape = Sig_eps_prior_params$IG_shape[j], scale = Sig_eps_prior_params$IG_scale[j])
+    }
+    return(diag(sig2_eps_vars))
+  }
   
 }
 
 
-sample_Sig_eps <- function(model_errs, Sig_eps_prior_params, n) {
+sample_cond_post_Sig_eps <- function(model_errs, Sig_eps_prior_params, n) {
   # Return sample of the observation covariance matrix Sig_eps, drawn from the conditional  
   # posterior p(Sig_eps|theta, Y). Under the model assumptions, this conditional posterior 
-  # has an inverse Wishart distribution. This function does not explicitly take theta as an 
+  # has an inverse Wishart or Inverse Gamma product distribution. This function does not explicitly take theta as an 
   # argument; rather, it assumes the forward model f(theta) has already been run and 
   # the error matrix Y - f(theta) is provided by the argument `model_errs`.
   #
   # Args:
-  #    model_errs: matrix of dimensions n x p, where n = number observations in time 
-  #                 series and p = number output variables. 
-  #    Sig_eps_prior_params: list, containing elements with names "scale_matrix" and "dof" 
-  #                          that are the arguments of the Inverse Wishart distribution.
+  #     model_errs: matrix of dimensions n x p, where n = number observations in time 
+  #                 series and p = number output variables. This is the model error matrix
+  #                 Y - f(theta). 
+  #    Sig_eps_prior_params: list, must contain element named "dist" specifying the prior distribution. Current accepted values are 
+  #                          "IW" (Inverse Wishart) or "IG" (independent Inverse Gamma priors). Depending on value of "dist", 
+  #                          must also contain either either 1.) names "scale_matrix" and "dof" 
+  #                          that are the arguments of the Inverse Wishart distribution, or 2.) names "IG_shape" and "IG_scale" which 
+  #                          each correspond to p-length vectors storing the parameters for the independent Inverse Gamma priors on each
+  #                          variance parameter. Note that dist "IG" constrains Sig_eps to be diagonal, while "IW" does not. 
   #    n: The number of observations in Y (i.e. the length of the time series). Should 
   #       equal the number of rows of `model_errs`.
   #
   # Returns:
-  #    matrix, p x p positive definite matrix sampled from the inverse Wishart distribution p(Sig_eps|theta, Y).
+  #    matrix, p x p positive definite matrix sampled from the prior distribution p(Sig_eps|theta, Y).
   
-  inv_wishart_scale <- crossprod(model_errs_curr) + Sig_eps_prior_params$scale_matrix
-  inv_wishart_dof <- n + Sig_eps_prior_params$dof
-  
-  return(LaplacesDemon::rinvwishart(nu = inv_wisharat_dof, S = inv_wishart_scale))
+  if(Sig_eps_prior_params$dist == "IW") {
+    inv_wishart_scale <- crossprod(model_errs_curr) + Sig_eps_prior_params$scale_matrix
+    inv_wishart_dof <- n + Sig_eps_prior_params$dof
+    return(LaplacesDemon::rinvwishart(nu = inv_wisharat_dof, S = inv_wishart_scale))
+  } else if(Sig_eps_prior_params$dist == "IG") {
+    n <- nrow(model_errs)
+    p <- ncol(model_errs)
+    output_l2_errs <- colsums(model_errs^2)
+    sig2_eps_vars <- vector(mode = "numeric", length = p)
+    for(j in seq_len(p)) {
+      sig2_eps_vars[j] <- LaplacesDemon::rinvgamma(1, shape = 0.5*n + Sig_eps_prior_params$IG_shape[j], scale = 0.5*output_l2_errs[j] + Sig_eps_prior_params$IG_scale[j])
+    }
+    return(diag(sig2_eps_vars))
+  }
   
 }
 
