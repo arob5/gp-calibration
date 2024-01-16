f1c46079bc891cfa7bee1f329e1a254e115e7029
Andrew Roberts <agr.roberts@gmail.com>
2024-01-16 16:35:56 -0500

    Added scaling and normalization functions to the gpWrapper class.

--- src/gp.r
+++ src/gp.r
@@ -12,8 +12,53 @@
 
 gpWrapper <- setRefClass(
    Class = "gpWrapper", 
-   fields = list(gp_model="ANY", lib="character", X="matrix", y="numeric")
+   fields = list(gp_model="ANY", lib="character", X="matrix", Y="numeric", 
+                 X_dim="integer", Y_dim="integer",
+                 scale_input="logical", scale_output="logical",
+                 input_bounds="matrix", Y_mean="numeric", Y_std="numeric",
+                 X_names="character", Y_names="character",
+                 X_std="matrix", Y_norm="matrix")
 )
+
+gpWrapper$methods(
+  
+  scale_input = function(Xnew, inverse=FALSE) {
+    if(inverse) {
+      Xnew <- Xnew %*% diag(input_bounds[2,] - input_bounds[1,], X_dim) + 
+              matrix(input_bounds[1,], nrow=nrow(Xnew), ncol=X_dim, byrow=TRUE)
+    } else {
+      Xnew <- (Xnew - matrix(input_bounds[1,], nrow=nrow(Xnew), ncol=X_dim, byrow=TRUE)) %*% 
+              diag(1/(input_bounds[2,] - input_bounds[1,]), X_dim)
+    }
+    
+    return(Xnew)
+  }, 
+  
+  scale_output = function(Ynew, inverse=FALSE) {
+    if(inverse) {
+      Ynew <- Ynew * matrix(Y_std, nrow=nrow(Ynew), ncol=Y_dim, byrow=TRUE) + 
+              matrix(Y_mean, nrow=nrow(Ynew), ncol=Y_dim, byrow=TRUE)
+    } else {
+      Ynew <- (Ynew - matrix(Y_mean, nrow=nrow(Ynew), ncol=Y_dim, byrow=TRUE)) / 
+              matrix(Y_std, nrow=nrow(Ynew), ncol=Y_dim, byrow=TRUE)
+    }
+    
+    return(Ynew)
+  }
+  
+)
+
+
+
+
+
+# Methods to add: 
+#    get_input_dim()
+#    get_num_design()
+#    get_output_dim() (for the indGPWrapper class)
+#    get_pred_quantiles()
+#    predict_parallel() (for indGPWRapper)
+#    fit() (maybe standardize some inputs to fit method but allow it to be pretty flexible)
 
 # -----------------------------------------------------------------------------
 # gpHet: Class providing interface between calibration code and hetGP package.  
