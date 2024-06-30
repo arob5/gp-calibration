a744138ea6cb26fc80a2d4b9c97533d23e3fa60e
Andrew Roberts <arober@bu.edu>
2024-06-30 15:43:08 -0400

    More adjustments to command line argument reader.

--- scripts/gp_post_approx_paper/bash_scripts/run_multidim_toy_example.sh
+++ scripts/gp_post_approx_paper/bash_scripts/run_multidim_toy_example.sh
@@ -10,11 +10,10 @@ module load gcc/8.3.0
 module load R/4.3.1
 
 # Ensure the R project library for project `gp-calibration` is being used. 
-export R_LIBS="/projectnb/dietzelab/arober/gp-calibration/renv/library/R-4.3/x86_64-pc-linux-gnu"
+# export R_LIBS="/projectnb/dietzelab/arober/gp-calibration/renv/library/R-4.3/x86_64-pc-linux-gnu"
 
 # Required for the `Rscript` command to be found. 
 export PATH="$PATH:/share/pkg.8/r/4.3.1/install/lib64/R"
 
-Rscript ../run_multidim_toy_example.r
-
+Rscript ../run_multidim_toy_example.r linGauss --dim_par=3 --dim_output=10 --N_design=30 --N_design_test=400
 
